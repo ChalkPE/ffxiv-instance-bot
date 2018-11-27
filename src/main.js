1896554ccf1e4b5a0faaaf59f8435cad7779f84c
Chalk <chalkpe@gmail.com>
2018-11-27 15:10:40 +0900

    add global, message cases

--- src/main.js
+++ src/main.js
@@ -56,18 +56,12 @@
     let payload = event.detail.payload
 
     console.log(opcode, payload)
+
     switch (opcode) {
-      case 0:
-        return onChatMessage(payload)
-
-      case 1:
-        return onZoneChange(payload)
-
-      case 2:
-        return onMeSpawned(payload)
-
-      case 3:
-        return onEntitySpawned(payload)
+      case 0: return onChatMessage(payload)
+      case 1: return onZoneChanged(payload)
+      case 2: return onMeSpawned(payload)
+      case 3: return onEntitySpawned(payload)
     }
   }
 
@@ -77,15 +71,22 @@
     let message = payload[2]
 
     switch (type) {
-      case '0010': break //1번 링크셸
-      case '0011': break //2번 링크셸
-      case '0012': break //3번 링크셸
-      case '0013': break //4번 링크셸
-      case '0018': break //자유부대
+      case '0003': break // 시스템 공지
+      case '0010': break // 1번 링크셸
+      case '0011': break // 2번 링크셸
+      case '0012': break // 3번 링크셸
+      case '0013': break // 4번 링크셸
+      case '0018': break // 자유부대
+      case '001c': break // 감정 표현 (명령어)
+      case '001d': break // 감정 표현 (일반)
+      case '0039': break // 시스템 메세지
+
+      case '0047': break // 장터 판매 알림?
+      case '0048': break // 파티 모집 알림?
     }
   }
 
-  function onZoneChange (payload) {
+  function onZoneChanged (payload) {
     let id = payload[0]
     let name = payload[1]
 
@@ -109,13 +110,16 @@
     let id = payload[0]
     let name = payload[1]
     let job = payload[2]
-    let level = parseInt(payload[3], 16)
+    let level = payload[3]
 
     if (id === profile.id) {
-      profile.job = job
-      profile.level = level
+      profile.job = JOB_CODES[job] || `Unknown (${job})`
+      profile.level = parseInt(level, 16)
     }
   }
+
+  window.zone = zone
+  window.profile = profile
 
   document.addEventListener('onLogLine', onLogLine)
 })()
