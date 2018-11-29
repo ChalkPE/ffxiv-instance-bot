ff36a8229584266ebca5f45ba4a68d6c3a3a9acc
whiteWeed <wsd97931@gmail.com>
2018-11-29 14:36:14 +0900

    added onBuffed() function.

--- overlay/main.js
+++ overlay/main.js
@@ -32,6 +32,7 @@
       case 1: return onZoneChanged(payload)
       case 2: return onMeSpawned(payload)
       case 3: return onEntitySpawned(payload)
+      case 26: return onBuffed(payload)
     }
   }
 
@@ -109,6 +110,23 @@
       profile.job = JOB_CODES[job] || `Unknown (${job})`
       profile.level = parseInt(level, 16)
     }
+  }
+
+  function onBuffed (payload) {
+    let id = payload[0]
+    let card = payload[1]
+    let duration = payload[2]
+    let from = {
+      id: payload[3],
+      name: payload[4],
+      health: payload[9]
+    }
+    let to = {
+      id: payload[5],
+      name: payload[6],
+      health: payload[8]
+    }
+    let stack = payload[7]
   }
 
   function openConfig () {
